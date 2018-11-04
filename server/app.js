c2bd1871d895a350dc24c6d28c424d415d84331e
r00bal <hal9000@MacBook-Pro-r00bal.local>
2018-11-04 19:22:42 +0100

    add mongodb

--- server/app.js
+++ server/app.js
@@ -1,13 +1,27 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const schema = require('./schema/schema')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const { user, pass } = require("./credentils.js");
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+
+// connect to mlab database
+mongoose.connect(
+  `mongodb://${user}:${pass}@ds249623.mlab.com:49623/gql-r00bal`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
-  console.log('now listening for request on port 4000');
+  console.log("now listening for request on port 4000");
 });
