5b51c8c2dadc97efd195d8bc85ad03a974f72f2d
r00bal <hal9000@MacBook-Pro-r00bal.local>
2018-11-23 18:27:43 +0100

    add bookDetails component

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -3,7 +3,17 @@ import { gql } from "apollo-boost";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
 
+// components
+
+import BookDetails from "./BookDetails";
+
 class BookList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
   displayBooks = () => {
     var data = this.props.data;
 
@@ -11,7 +21,16 @@ class BookList extends Component {
       return <div>Loading books...</div>;
     } else {
       return data.books.map(book => {
-        return <li key={book.id}>{book.name}</li>;
+        return (
+          <li
+            key={book.id}
+            onClick={e => {
+              this.setState({ selected: book.id });
+            }}
+          >
+            {book.name}
+          </li>
+        );
       });
     }
   };
@@ -20,6 +39,7 @@ class BookList extends Component {
     return (
       <div>
         <ul id="book-list">{this.displayBooks()}</ul>
+        <BookDetails bookid={this.state.selected} />
       </div>
     );
   }
